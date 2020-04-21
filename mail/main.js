519a84f560cbbed8e4c3cdddd5550e3008c2dd78
Gabriele Mercurio <gabrielemercurio.web@gmail.com>
2020-04-21 16:16:35 +0200

    Creazione Cartelle Separate

---
+++ mail/main.js
@@ -0,0 +1,13 @@
+// Chiedi all'utente la sua email
+var mailUtente = prompt ('Scrivi qua la tua Email:');
+console.log(mailUtente);
+
+// Creare un array contenente le email "autorizzate"
+var autorizzati = ['pippo@gmail.com', 'pluto@gmail.com', 'topolino@gmail.com', 'paperino@gmail.com', 'paperina@gmail.com'];
+console.log([autorizzati]);
+
+// Controlla che sia nella lista di chi può accedere
+// e stampa un messaggio appropriato (sei in lista/non sei in lista)
+if (mailUtente) {
+
+}
